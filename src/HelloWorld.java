0725b2990228fa011486435413dea0978c859a33
byGaoxw <276180941@qq.com>
2020-06-03 17:15:08 +0800

    第2次提交上去的代码

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world！！！");
         System.out.println("第一次提交git");
+        System.out.println("第2次提交git");
     }
 }
